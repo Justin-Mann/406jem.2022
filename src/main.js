1a609cfeb4e26ca0e466f2e9503dfd8677eab499
Justin <justinmann.mail@gmail.com>
2022-05-12 10:51:40 -0600

    app param value update

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from './406jemApp.svelte';
 const app = new App({
 	target: document.body,
 	props: {
-		appname: '406jem.us',
+		appname: '406jem',
 		biglogoimagepath: './img/samurai.png',
 		currentresumeuri: './dox/resume.printversion.pdf'
 	}
